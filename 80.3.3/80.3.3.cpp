f897fbccb9aea3b9ffa0fc84306a6a7306783e43
b a <ab@wanwangzi.onmicrosoft.com>
2022-04-21 13:18:38 +0800

    添加项目文件。

---
+++ 80.3.3/80.3.3.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float x, y;
+    printf("第一边");
+    scanf_s("%d", &x);
+    printf("第二边");
+    scanf_s("%d", &y);
+    if (x<0)
+    {
+        x* x;
+    }
+    if (0<=x<10)
+    {
+        -0.5 * x + 10;
+    }
+    if (x>=10)
+    {
+        x - sqrt(x);
+    }
